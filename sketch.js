7846563e6026f64749e014e1b1b9f489590e4497
Tian Jiang <jiang925@hotmail.com>
2019-06-10 22:58:05 -0400

    Better maze generation algorithm

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const RESOLUTION = 60;
-const BLOCK_DENSITY = 0.8;
+const RESOLUTION = 600/10;
 const RANDOM_MOVE_CHANCE = 0.05;
 const DEBUG = false;
 const LEARNING_RATE = 0.2;
@@ -11,10 +10,11 @@ const FRAME_RATE = 30;
 
 let grid;
 let mouse;
+let worldGenerator;
 const rand = new Math.seedrandom(99);
+const wgrand = new Math.seedrandom(100);
 const wrand = new Math.seedrandom(101);
 const mrand = new Math.seedrandom(102);
-const inGrid = (x, y) => (grid.length && x < grid.length && x >= 0 && grid[x].length && y < grid[x].length && y >=0);
 let generation;
 let bestSteps;
 let generationDiv;
@@ -26,29 +26,26 @@ class Cell {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    if (rand() > BLOCK_DENSITY) {
-      this.block = true;
-    }
     this.neighbors = [];
     this.table = [];
   }
-  
+
   addNeighbor(c) {
     this.neighbors.push(c);
     this.table.push(INITIAL_WEIGHT);
   }
-  
+
   updateNeighbor(n, w) {
     const i = this.neighbors.indexOf(n);
     if (DEBUG) console.log('update neighor', n, 'index', i, 'to weight', w);
     if (i >= 0) this.table[i] = w;
   }
-  
+
   getNeighborWeight(n) {
     const i = this.neighbors.indexOf(n);
     return i >= 0 ? this.table[i] : 0;
   }
-  
+
   bestNeighbor(excludes = []) {
     if (!this.neighbors.length) return null;
     let b;
@@ -60,17 +57,26 @@ class Cell {
     }
     return b;
   }
-  
+
   draw() {
-    stroke(255, 255, 255);
-    if (this.block) {
-      fill(255);
-    } else {
-      noFill();
-    }
+    stroke(255);
+    noFill();
     rect(this.x * RESOLUTION, this.y * RESOLUTION, RESOLUTION, RESOLUTION);
-    
+    this.neighbors.forEach(n => {
+      stroke(0);
+      const diffX = n.x - this.x;
+      const diffY = n.y - this.y;
+      if (DEBUG) console.log('erase wall between ', this, 'and', n);
+      line(
+        this.x * RESOLUTION + (diffX == 1 ? RESOLUTION : 0),
+        this.y * RESOLUTION + (diffY == 1 ? RESOLUTION : 0),
+        this.x * RESOLUTION + (diffX == -1 ? 0 : RESOLUTION),
+        this.y * RESOLUTION + (diffY == -1 ? 0 : RESOLUTION)
+      );
+    });
+
     textAlign(CENTER);
+    stroke(255);
     if (this.target) {
       text('cheese', this.x * RESOLUTION + RESOLUTION/2, this.y * RESOLUTION + RESOLUTION/2 + 2);
     } else {
@@ -86,28 +92,28 @@ class Mouse {
     this.y = y;
     this.path = [];
   }
-  
+
   draw() {
     textAlign(CENTER);
     text('M', this.x * RESOLUTION + RESOLUTION/2, this.y * RESOLUTION + RESOLUTION/4);
-    
+
     for (let p of this.path) {
       text('m', p.x * RESOLUTION + RESOLUTION/2, p.y * RESOLUTION + RESOLUTION/4);
     }
   }
-  
+
   move() {
     // maintain path
     if (this.dead) return;
     if (DEBUG) console.log('mouse', this);
     const c = grid[this.x][this.y];
     this.path.push(c);
-    
+
     // get the neighbors
     let b = c.bestNeighbor(this.path);
-    
+
     if (DEBUG) console.log('best neighbor', b);
-    
+
     // check if we have died
     if (this.path.includes(b) || !b) {
       this.dead = true;
@@ -119,12 +125,61 @@ class Mouse {
       if (DEBUG) console.log('REACHED');
       return;
     }
-    
+
     // move the mouse
     const i = Math.trunc(mrand() * c.neighbors.length);
     const n = mrand() < RANDOM_MOVE_CHANCE ? c.neighbors[i] : b;
     this.x = n.x;
     this.y = n.y;
+  }
+}
+
+class WorldGenerator {
+  constructor(rng) {
+    this.rng = rng;
+  }
+
+  // return a 2d array of Cell, each has a list of neighbors generated randomly
+  // nx and ny are number of cells in x and y directions
+  generate(nx, ny) {
+    // generate empty grid first
+    let grid = [];
+    for (let i = 0; i < nx; i++) {
+      grid.push([]);
+      for (let j = 0; j < ny; j++) {
+        grid[i].push(new Cell(i, j));
+      }
+    }
+
+    const hash = c => c.x + ',' + c.y;
+    const inGrid = (x, y) => (grid.length && x < grid.length && x >= 0 && grid[x].length && y < grid[x].length && y >=0);
+    const getNeighbors = (grid, x, y) => [[x-1,y],[x,y-1],[x+1,y],[x,y+1]].filter(coord => inGrid(coord[0], coord[1])).map(coord => grid[coord[0]][coord[1]]);
+    const getUnvisitedNeighbors = (visited, grid, x, y) => getNeighbors(grid, x, y).filter(c => !visited[hash(c)]);
+
+    let stack = [];
+    let visited = {}; // "{x},{y}"
+    let current = grid[0][0];
+    const totalCells = nx * ny;
+
+    visited[hash(current)] = true;
+
+    while(Object.keys(visited).length < totalCells) {
+      const unvisitedN = getUnvisitedNeighbors(visited, grid, current.x, current.y);
+      if (unvisitedN.length) {
+        const randomN = unvisitedN[Math.trunc(unvisitedN.length * this.rng())];
+        stack.push(current);
+        current.addNeighbor(randomN);
+        randomN.addNeighbor(current);
+        current = randomN;
+        visited[hash(current)] = true;
+      } else if (stack.length) {
+        current = stack.pop();
+      }
+    }
+
+    grid[nx-1][ny-1].target = true;
+
+    return grid;
   }
 }
 
@@ -152,39 +207,15 @@ function setup() {
   generationDiv = createDiv();
   bestStepsDiv = createDiv();
   frameRateDiv = createDiv();
+  worldGenerator = new WorldGenerator(wgrand);
   init();
 }
 
 function init() {
   const nx = 600 / RESOLUTION;
   const ny = 600 / RESOLUTION;
-  grid = [];
-  for (let i = 0; i < nx; i++) {
-    grid.push([]);
-    for (let j = 0; j < ny; j++) {
-      grid[i].push(new Cell(i, j));
-    }
-  }
-  for (let gs of grid) {
-    for (let cell of gs) {
-      const i = cell.x;
-      const j = cell.y;
-      if (inGrid(i - 1, j) && !grid[i-1][j].block) {
-        cell.addNeighbor(grid[i-1][j]);
-      }
-      if (inGrid(i, j - 1) && !grid[i][j-1].block) {
-        cell.addNeighbor(grid[i][j-1]);
-      }
-      if (inGrid(i + 1, j) && !grid[i+1][j].block) {
-        cell.addNeighbor(grid[i+1][j]);
-      }
-      if (inGrid(i, j + 1) && !grid[i][j+1].block) {
-        cell.addNeighbor(grid[i][j+1]);
-      }
-    }
-  }
-  
-  grid[nx-1][ny-1].target = true;
+
+  grid = worldGenerator.generate(nx, ny);
   mouse = new Mouse(0, 0);
   generation = 0;
   tick();
@@ -196,7 +227,7 @@ function tick() {
     learn();
     mouse = new Mouse(0, 0);
   }
-  
+
   mouse.move();
   ticking = false;
   setTimeout(tick, 1);
@@ -204,13 +235,13 @@ function tick() {
 
 function draw() {
   background(0);
-  
+
   generationDiv.html('Generation: ' + generation);
   bestStepsDiv.html('Best Steps: ' + (bestSteps || 'N/A'));
   frameRateDiv.html('Frame rate: ' + frameRate().toFixed(0));
-  
+
   if (ticking) return;
-  
+
   for (let gs of grid) {
     for (let cell of gs) {
       cell.draw();
